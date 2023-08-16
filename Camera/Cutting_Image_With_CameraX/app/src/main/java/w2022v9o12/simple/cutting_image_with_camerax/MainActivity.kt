899997a175ee6cc418abefd5466dc12f4347ec74
leegyeongjun <augustin26@e9pay.co.kr>
2023-08-16 14:46:39 +0900

    Adding serveral parts

---
+++ Camera/Cutting_Image_With_CameraX/app/src/main/java/w2022v9o12/simple/cutting_image_with_camerax/MainActivity.kt
@@ -0,0 +1,11 @@
+package w2022v9o12.simple.cutting_image_with_camerax
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
