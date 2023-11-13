e4d385770b20576a43826142c1fd2f6c42ce2b80
tvroom88 <tvroom88@naver.com>
2023-11-13 18:04:31 +0900

    edit a little bit

--- Camera/CameraX_2_takePicture/app/src/main/java/com/example/camerax_1/ResultActivity.kt
+++ Camera/CameraX_2_takePicture/app/src/main/java/com/example/camerax_1/ResultActivity.kt
@@ -1,5 +1,7 @@
 package com.example.camerax_1
 
+import android.graphics.Bitmap
+import android.graphics.Matrix
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,7 +12,33 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
+        var mBitmap = (applicationContext as MyApplication).bitmap
+
+        if(mBitmap!!.width > mBitmap?.height!!){
+            mBitmap =  rotateTheImage(90, mBitmap)
+        }
+
         val imageView = findViewById<ImageView>(R.id.imageView)
-        imageView.setImageBitmap((applicationContext as MyApplication).bitmap )
+        imageView.setImageBitmap(mBitmap)
+    }
+
+
+    fun rotateTheImage(orientation: Int, oldBitmap: Bitmap): Bitmap? {
+        val newBitmap: Bitmap
+        if (orientation != 0) {
+            val matrix = Matrix()
+            matrix.postRotate(orientation.toFloat())
+            newBitmap = Bitmap.createBitmap(
+                oldBitmap,
+                0,
+                0,
+                oldBitmap.width,
+                oldBitmap.height,
+                matrix,
+                false
+            )
+            return newBitmap
+        }
+        return oldBitmap
     }
 }
