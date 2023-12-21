e36d9afb40923bafd4e640f0c57f3caaa98dbb68
tvroom88 <tvroom88@naver.com>
2023-12-21 14:56:06 +0900

    adding foreground servie example

--- RemoteControl/Extra_Practice/ForegroundService/app/src/main/java/com/example/foregroundservice/MainActivity.kt
+++ RemoteControl/Extra_Practice/ForegroundService/app/src/main/java/com/example/foregroundservice/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.foregroundservice
 
 import android.Manifest
+import android.app.ActivityManager
+import android.app.ActivityManager.AppTask
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +24,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnStartService: Button
     private lateinit var btnStopService: Button
     private lateinit var changeNotiBtn: Button
+    private lateinit var checkRunningServiceBtn: Button
+    private lateinit var startProgress: Button
+    private lateinit var stopProgress: Button
+
+    private lateinit var titleET: EditText
+    private lateinit var contentET: EditText
 
     private lateinit var requestNotificationPermission: ActivityResultLauncher<String>
 
@@ -31,8 +41,18 @@ class MainActivity : AppCompatActivity() {
         serviceIntent = Intent(this, SimpleForegroundService::class.java)
 
         btnStartService = findViewById(R.id.buttonStartService)
+
         btnStopService = findViewById(R.id.buttonStopService)
+
+        startProgress = findViewById(R.id.startProgress)
+
+        stopProgress = findViewById(R.id.stopProgress)
+
+        checkRunningServiceBtn = findViewById(R.id.checkRunningServiceBtn)
+
         changeNotiBtn = findViewById(R.id.changeNotiBtn)
+        titleET = findViewById(R.id.titleET)
+        contentET = findViewById(R.id.contentET)
 
         btnStartService.setOnClickListener {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
@@ -50,8 +70,37 @@ class MainActivity : AppCompatActivity() {
         }
         btnStopService.setOnClickListener { SimpleForegroundService.stopService(this) }
 
-        changeNotiBtn.setOnClickListener{
-            SimpleForegroundService.sendMessageToService(this, "new title","new content")
+        startProgress.setOnClickListener {
+            if (isServiceRunning(this))
+                SimpleForegroundService.startProgress(this)
+            else
+                Toast.makeText(this, "Foreground Service를 켜고 시도해주세요.", Toast.LENGTH_SHORT).show()
+
+        }
+
+        stopProgress.setOnClickListener {
+            if (isServiceRunning(this))
+                SimpleForegroundService.stopProgress(this)
+            else
+                Toast.makeText(this, "Foreground Service를 켜고 시도해주세요.", Toast.LENGTH_SHORT).show()
+        }
+
+        changeNotiBtn.setOnClickListener {
+            if (isServiceRunning(this))
+                SimpleForegroundService.sendMessageToService(
+                    this,
+                    titleET.text.toString(),
+                    contentET.text.toString()
+                )
+            else
+                Toast.makeText(this, "Foreground Service가 켜져있지 않습니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        checkRunningServiceBtn.setOnClickListener {
+            if (isServiceRunning(this))
+                Toast.makeText(this, "Foreground Service가 켜져있습니다.", Toast.LENGTH_SHORT).show()
+            else
+                Toast.makeText(this, "Foreground Service가 켜져있지 않습니다.", Toast.LENGTH_SHORT).show()
         }
 
         requestNotificationPermission =
@@ -79,12 +128,22 @@ class MainActivity : AppCompatActivity() {
             ContextCompat.checkSelfPermission(this, permission) -> {
                 true
             }
-
             else -> {
                 false
             }
         }
     }
+
+    private fun isServiceRunning(context: Context): Boolean {
+        val am = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (rsi in am.getRunningServices(Integer.MAX_VALUE)) {
+            if (SimpleForegroundService::class.java.name == rsi.service.className) {
+                return true
+            }
+        }
+        return false
+    }
+
 
     companion object {
         @RequiresApi(Build.VERSION_CODES.TIRAMISU)
--- RemoteControl/Extra_Practice/ForegroundService/app/src/main/java/com/example/foregroundservice/SimpleForegroundService.kt
+++ RemoteControl/Extra_Practice/ForegroundService/app/src/main/java/com/example/foregroundservice/SimpleForegroundService.kt
@@ -1,6 +1,5 @@
 package com.example.foregroundservice
 
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -17,14 +16,24 @@ class SimpleForegroundService : Service() {
 
 
     private var TAG = "SimpleForegroundService"
-    private var startMode: Int = 0             // 서비스가 kill 될 때, 어떻게 동작할지를 나타냄
     private var binder: IBinder? = null        // bind 된 클라이언트와 소통하기 위한 인터페이스
-    private var allowRebind: Boolean = false   // onRebind() 메소드가 사용될지 말지를 결정함
-    private lateinit var notification: Notification
+
+    private lateinit var notification: NotificationCompat.Builder
+    private lateinit var manager: NotificationManager
+
+    private var progressThread: Thread? = null
+    private var startFlag = true
+    private var threadIsRunning = true // Thread가 여려개 켲지는 것을 막기 위해서 
+
+
+    var startProgressPoint = 0
+
     override fun onCreate() {
         // 서비스가 생성 될 때
-        Log.d(TAG, "ForegroundService - onCreate")
         super.onCreate()
+        Log.d(TAG, "ForegroundService - onCreate")
+
+//        manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
@@ -45,87 +54,138 @@ class SimpleForegroundService : Service() {
                     //안드로이드 sdk 26버전 이상에서는 알림창을 띄워야 Foreground
                     createNotificationChannel()
 
-                    if (inputTitle != null && inputContent != null) {
-                        startNewService(inputTitle, inputContent)
+                    // Notification 클릭시 MainActivity 이동
+                    val notificationIntent = Intent(this, MainActivity::class.java)
+                    val pendingIntent = PendingIntent.getActivity(
+                        this,
+                        0,
+                        notificationIntent,
+                        PendingIntent.FLAG_IMMUTABLE
+                    )
+
+                    //  Notification 클릭시 SimpleForegroundService 이동. Progress Bar 작동 시작
+                    val startProgressIntent = Intent(this, SimpleForegroundService::class.java)
+                    startProgressIntent.action = ("StartProgress")
+                    val startProgressPendingIntent = PendingIntent.getService(
+                        this,
+                        0,
+                        startProgressIntent,
+                        PendingIntent.FLAG_MUTABLE
+                    )
+
+                    // Progress 정지
+                    val stopProgressIntent = Intent(this, SimpleForegroundService::class.java)
+                    stopProgressIntent.action = ("StopProgress")
+                    val stopProgressPendingIntent = PendingIntent.getService(
+                        this,
+                        0,
+                        stopProgressIntent,
+                        PendingIntent.FLAG_MUTABLE
+                    )
+
+
+                    // 버튼 클릭시 Foreground Service 종료
+                    val stopServiceIntent = Intent(this, SimpleForegroundService::class.java)
+                    stopServiceIntent.action = ("StopService")
+                    val stopPendingIntent = PendingIntent.getService(
+                        this,
+                        0,
+                        stopServiceIntent,
+                        PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_MUTABLE
+                    )
+
+
+                    notification = NotificationCompat.Builder(this, CHANNEL_ID).apply {
+                        setSmallIcon(R.drawable.coffee_icon)
+                        setContentTitle(inputTitle)
+                        setContentText(inputContent)
+                        setProgress(100, 0, false)
+                        setStyle(
+                            NotificationCompat.BigTextStyle()
+                                .bigText(inputContent)
+                        )
+                        addAction(R.drawable.start, "start progress", startProgressPendingIntent)
+                        addAction(R.drawable.cancel, "stop progress", stopProgressPendingIntent)
+                        addAction(R.drawable.cancel, "stop service", stopPendingIntent)
+                        setContentIntent(pendingIntent)
                     }
+
+                    startForeground(1, notification.build())
+
+                }
+
+                // Progess 시작
+                "StartProgress" -> {
+                    Log.d(TAG, "ForegroundService - StartProgress")
+                    startProgress()
+                }
+
+                // Progress 정지
+                "StopProgress" -> {
+                    Log.d(TAG, "ForegroundService - StopProgress")
+                    stopProgress()
                 }
 
                 "StopService" -> {
                     Log.d(TAG, "ForegroundService - stopSelf")
 
                     // 서비스를 정지시키는 로직을 구현합니다.
-                    stopForeground(STOP_FOREGROUND_DETACH)
+                    stopForeground(STOP_FOREGROUND_REMOVE)
                     stopSelf()
                 }
 
+                // Notification 제목과 내용 바꿀때
                 "getMessageFromActivity" -> {
-                    
                     val inputTitle = intent.getStringExtra(Noti_Title)
                     val inputContent = intent.getStringExtra(Noti_Content)
-
-                    if (inputTitle != null && inputContent != null) {
-                        startNewService(inputTitle, inputContent)
-                    }
+                    notification.setContentTitle(inputTitle)
+                    notification.setContentText(inputContent)
+                    notification.setStyle(NotificationCompat.BigTextStyle().bigText(inputContent))
+                    manager.notify(1, notification.build())
                 }
+
             }
         }
 
         return START_STICKY
     }
 
-    private fun startNewService(inputTitle: String, inputContent: String) {
-        // Notification 클릭시 MainActivity 이동
-        val notificationIntent = Intent(this, MainActivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(
-            this,
-            0,
-            notificationIntent,
-            PendingIntent.FLAG_IMMUTABLE
-        )
+    private fun startProgress() {
 
-        // 버튼 클릭시 Foreground Service 종료
-        val stopServiceIntent = Intent(this, SimpleForegroundService::class.java)
-        stopServiceIntent.action = ("StopService")
-        val stopPendingIntent = PendingIntent.getService(
-            this,
-            0,
-            stopServiceIntent,
-            PendingIntent.FLAG_MUTABLE
-        )
+        progressThread = Thread {
+            for (incr in startProgressPoint..100 step 5) {
+                if (startFlag) {
+                    notification.setProgress(100, incr, false)
+                    startProgressPoint = incr
+                    manager.notify(1, notification.build())
+                    try {
+                        Thread.sleep(1 * 500.toLong())
+                    } catch (e: InterruptedException) {
+                        Log.d("TAG", "sleep failure")
+                    }
+                } else {
+                    startFlag = true
+                    break
+                }
+            }
+            // When the loop is finished, updates the notification
+            notification.setContentText("Download completed")
+            manager.notify(1, notification.build())
+        }
+        if (threadIsRunning) { // Thread가 한번 켜지면 더 실행되지 않게 하기 위해서
+            progressThread!!.start()
+            threadIsRunning = false
+        }
 
-
-        notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle(inputTitle)
-            .setContentText(inputContent)
-            .setSmallIcon(R.drawable.coffee_icon)
-            .addAction(R.drawable.cancel, "stop service", stopPendingIntent)
-            .setContentIntent(pendingIntent)
-            .build()
-
-        startForeground(1, notification)
     }
 
-    override fun onBind(intent: Intent): IBinder? {
-        // bindService()에 의해 서비스가 시작될 때
-        Log.d("ForegroundService", "ForegroundService - onBind")
-        return binder
-    }
-
-    override fun onUnbind(intent: Intent): Boolean {
-        // 모든 클라이언트가 unbindService()를 호출했을 때. 즉, 더이상 바인딩된 클라이언트가 없을때
-        Log.d("ForegroundService", "ForegroundService - onUnbind")
-        return allowRebind
-    }
-
-    override fun onRebind(intent: Intent) {
-        // onUnbind()가 호출된적이 있는 상태에서, 다시 bindService()를 통해 바인딩 할 때
-        Log.d("ForegroundService", "ForegroundService - onRebind")
-    }
-
-    override fun onDestroy() {
-        // 서비스가 종료할 때
-        Log.d("ForegroundService", "ForegroundService - onDestroy")
-
+    private fun stopProgress() {
+        if (startFlag && progressThread != null) {
+            startFlag = false
+            threadIsRunning = true // Thread가 정지 되면 다시 Thread가 실행되도 되게
+            progressThread!!.interrupt()
+            progressThread = null
+        }
     }
 
     private fun createNotificationChannel() {
@@ -134,8 +194,8 @@ class SimpleForegroundService : Service() {
                 CHANNEL_ID,
                 "Foreground Service Channel",
                 NotificationManager.IMPORTANCE_DEFAULT
-            )
-            val manager = getSystemService(
+            ).apply { description = "Foreground Service 예제" }
+            manager = getSystemService(
                 NotificationManager::class.java
             )
             manager.createNotificationChannel(serviceChannel)
@@ -175,5 +235,41 @@ class SimpleForegroundService : Service() {
             val intent = Intent(context, SimpleForegroundService::class.java)
             context.stopService(intent)
         }
+
+
+        fun startProgress(context: Context) {
+            val intent = Intent(context, SimpleForegroundService::class.java)
+            intent.action = "StartProgress"
+            if (Build.VERSION.SDK_INT < 26) {
+                context.startService(intent)
+            } else {
+                context.startForegroundService(intent)
+            }
+        }
+
+        fun stopProgress(context: Context) {
+            val intent = Intent(context, SimpleForegroundService::class.java)
+            intent.action = "StopProgress"
+            if (Build.VERSION.SDK_INT < 26) {
+                context.startService(intent)
+            } else {
+                context.startForegroundService(intent)
+            }
+        }
+
     }
+
+
+    override fun onBind(intent: Intent): IBinder? {
+        // bindService()에 의해 서비스가 시작될 때
+        Log.d("ForegroundService", "ForegroundService - onBind")
+        return binder
+    }
+
+    override fun onDestroy() {
+        // 서비스가 종료할 때
+        Log.d("ForegroundService", "ForegroundService - onDestroy")
+
+    }
+
 }
