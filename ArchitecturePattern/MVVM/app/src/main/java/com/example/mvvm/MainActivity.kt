e4e2f11ea5058cb83a4dca00605b0f99306fc363
tvroom88 <tvroom88@naver.com>
2024-01-03 09:40:41 +0900

    MVVM Pattern

---
+++ ArchitecturePattern/MVVM/app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.mvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.mvvm.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var mainViewModel = MainViewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.apply {
+            // **중요** binding에 LifeCycleOwner을 지정해줘야 LiveData가 실시간으로 변화
+            lifecycleOwner = this@MainActivity
+
+            // xml 파일에 선언한 viewModel
+            viewModel = mainViewModel
+        }
+    }
+
+    fun getRandomCharacter() {
+        mainViewModel.getRandomCharacter()
+    }
+}
---
+++ ArchitecturePattern/MVVM/app/src/main/java/com/example/mvvm/MyCharacter.kt
@@ -0,0 +1,3 @@
+package com.example.mvvm
+
+data class MyCharacter(var name: String, var strength: Int, var intelligence: Int)
